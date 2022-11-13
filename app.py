52c6e3b6a15ac561c4465d11f5fe9320e465957c
LoganLieou <logan.lieou@gmail.com>
2022-11-13 00:43:51 -0600

    complete send_user and detail

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify, request
 import sqlite3
 import numpy as np
+import requests
+import os
 
 app = Flask(__name__)
+
+cache = {}
 
 def similarity(to_cities, like_city):
     """
@@ -31,42 +35,67 @@ def similarity(to_cities, like_city):
         name = to_city[1]
         to_city = [type(x) == int or type(x) == float for x in to_city]
         res.append([name, 1 / (1 + np.exp(-1*np.dot(like_city, to_city)))])
-    return res
+    return res * 100
 
 @app.route("/detail")
 def detail():
-    return jsonify()
+    # return the cache front end can use it
+    return jsonify(cache)
 
-@app.route("/send_city", methods=["POST"])
+@app.route("/send_city", methods=["POST", "GET"])
 def send_preferences():
     if request.method == "POST":
         try:
+            # TODO replace with cockroach db connection
+            # deploy cockroach db using cockroach cloud
+            # just need actual connection string
             conn = sqlite3.connect("test.db")
 
+            # information that is being sent from the frontend
             like = request.form['like']
+            radius = request.form['radius']
             to = request.form['to']
 
-            # DONE
-            to_cities = conn.execute(f"""
-                SELECT * FROM cities 
-                WHERE seat = {to}
-            """).fetchall()
-
-            # TODO
+            # getting the city we want it to be like from the database
+            # to compare to
             like_city = conn.execute(f"""
-                SELECT * FROM cities 
-                WHERE name = {like}
+                SELECT * FORM cities WHERE name = {like}
             """).fetchone()
 
-            print(to_cities)
-            print(like_city)
+            # get the zip code of the city we're moving to
+            to_city_zip = conn.execute(f"""
+                SELECT zip FROM cities 
+                WHERE name = {to}
+            """).fetchall()
 
-            res = similarity(to_cities, like_city)
-            return jsonify(res)
+            # list of zipcodes using that one API
+            url = f"""
+                https://www.zipcodeapi.com/rest/{os.getenv("API_KEY")}/radius.json/{to_city_zip}/{radius}/mile
+            """
+            list_of_zipcodes = requests.get(url=url).json()
+
+            # very scuffed way lots of sql queries going on here
+            # this is O(n) queries instead of O(1) 😔
+            to_cities = []
+            for zipcode in list_of_zipcodes:
+                to_cities.append(conn.execute(f"""
+                    SELECT * FROM citites WHERE zip = {zipcode.zipcode}
+                """).fetchone())
+
+            # need the tuple index for the name of the city
+            # cache this
+            # TODO verify this works
+            cache[like_city[1]] = to_cities
+
+            # return similarity score json
+            return jsonify(similarity(to_cities, like_city))
 
         except Exception as e:
+            # catch exceptions
             return "ERROR: " + str(e)
     else:
+        # tell the user to send post request instead
+        # ig this is not possible since the endpoint won't receive get requests
         return "Try sending a post request instead..."
 
 @app.route("/")
